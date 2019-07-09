f68b449dea97a56a5dbf34e08bcc40cfc91bc633
ocxz <2252779530@qq.com>
2019-07-09 15:56:16 +0800

    添加项目文件。

---
+++ _01_前一天复习/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_前一天复习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
